c15da8838d9532da2e22305069141359561ff27e
Emil Sayilov <emil.sayilov@gmail.com>
2022-05-02 16:33:12 +0200

    Adds the tts.py file

---
+++ tts.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+# obtain path to "english.wav" in the same folder as this script
+from os import path
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "Testament.wav")
+
+
+
+# use the audio file as the audio source
+r = sr.Recognizer()
+
+
+i = 0
+
+
+with sr.AudioFile(AUDIO_FILE) as source:
+    while i < 803:
+        audio = r.record(source, duration = 240)  # read the entire audio file
+        texte = r.recognize_google(audio, language= "fr-FR")
+        print(texte)
+        i+=240
