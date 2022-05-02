6aeaa19546736facb2205e72ea419225bb085f1c
Emil <62512004+sayilovemil@users.noreply.github.com>
2022-05-02 18:33:55 +0200

    Update tts.py
    Makes some small changes

--- tts.py
+++ tts.py
@@ -1,18 +1,13 @@
 import speech_recognition as sr
 
-# obtain path to "english.wav" in the same folder as this script
+# obtain path to "file.extention" in the same folder as this script
 from os import path
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "Testament.wav")
-
-
 
 # use the audio file as the audio source
 r = sr.Recognizer()
 
-
 i = 0
-
-
 with sr.AudioFile(AUDIO_FILE) as source:
     while i < 803:
         audio = r.record(source, duration = 240)  # read the entire audio file
